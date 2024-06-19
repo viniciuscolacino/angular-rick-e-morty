cf8e78d049b6d34808dcf9b9833ebff5b6870e44
Vinicius Colacino <33551326+viniciuscolacino@users.noreply.github.com>
2024-06-19 00:38:43 -0300

    feat: animação do favorito e temas

--- src/app/components/characters-list-item-preview/characters-list-item-preview.component.ts
+++ src/app/components/characters-list-item-preview/characters-list-item-preview.component.ts
@@ -1,31 +1,66 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject, signal } from '@angular/core';
 import { CharactersService } from '@services/characters.service';
 import { NgOptimizedImage } from '@angular/common';
-import { combineLatest } from 'rxjs';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Character } from 'app/core/models/character';
 import { ModalDismissReasons, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from '@shared/modal/modal.component';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-characters-list-item-preview',
   standalone: true,
   imports: [NgOptimizedImage, FontAwesomeModule, ModalComponent],
   templateUrl: './characters-list-item-preview.component.html',
-  styleUrl: './characters-list-item-preview.component.scss'
+  styleUrl: './characters-list-item-preview.component.scss',
+  animations: [
+    trigger('animationFavorite', [
+      state('favoriteThisItem', style({
+        transform: 'scale(2)',
+        top: '-30px'
+      })),
+
+      //transition('* => animationFavorite', animate('1s'))
+      transition('* => favoriteThisItem', [
+        animate('1s', keyframes([
+          style({
+            display: 'block',
+            opacity: '0',
+            marginTop: '0',
+            zIndex: '2'
+          }),
+          style({
+            opacity: '1',
+            marginTop: '-10px',
+          }),
+          style({
+            opacity: '0',
+            marginTop: '-20px',
+          }),
+          style({
+            display: 'none',
+            zIndex: '-1'
+          })
+        ]))
+      ])
+    ])
+  ]
 })
 
 export class CharactersListItemPreviewComponent {
   @Input() public character: any;
   #charactersService = inject(CharactersService);
   #modalService = inject(NgbModal);
+  public animateFavorite = signal('');
 
   public addFavorite(character: Character): void {
     this.#charactersService.addFavorite(character);
+    this.animateFavorite.set('favoriteThisItem');
   }
 
   public removeFavorite(character: Character): void {
     this.#charactersService.removeFavorite(character);
+    this.animateFavorite.set('');
   }
 
   private opcoesModal: NgbModalOptions = {
