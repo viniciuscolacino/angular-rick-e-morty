f95870b002a655b11761f65a1ec587fd42478543
Vinicius Colacino <33551326+viniciuscolacino@users.noreply.github.com>
2024-06-20 00:39:02 -0300

    removed published files

--- dist/rick-e-morty/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JG23MZRX.mjs";import"./chunk-GYCG33IV.mjs";import"./chunk-G5YBWAXJ.mjs";import"./chunk-HJSTLAKB.mjs";import"./chunk-I4XBTHV6.mjs";import"./chunk-GNX7K4GQ.mjs";import"./chunk-UFIA264M.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
